95fec842f2c09fcae610504531917f9b2b535e38
Shreya Jain <jain74164@gmail.com>
2026-02-18 15:27:06 +0530

    Your commit message

---
+++ 2Darrays/rotatematrixby90.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r,c;
+    cin>> r>> c;
+    int mat[r][c];
+    for(int i=0;i<r;i++){
+        for (int j=0;j<c;j++){
+            cin>>mat[i][j];
+        }
+    }
+    
+}
---
+++ Arrays/2DArrays/diagonalelements.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[3][3];
+    
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+        cin>>arr[i][j];
+        }
+    }
+     for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+        if(i==j){
+             cout<< "Diagonal elements: "<<arr[i][j]<<" ";
+        }
+      }
+    }
+}
