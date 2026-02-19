3d6d0de5e78135ee46c564dacd4313ad854b59fc
Shreya Jain <jain74164@gmail.com>
2026-02-19 21:38:14 +0530

    Your commit message

--- 2Darrays/setmatrixzero.cpp
+++ 2Darrays/setmatrixzero.cpp
@@ -1,29 +1,54 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-    int r,c;
-    cout<<"Enter no of rows and cols: ";
-    cin>>r >> c;
-    int mat[r][c];
-    cout<<"Enter elements: ";
-    //input of matrix 
-    for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-            cin>>mat[i][j];
+
+void setZeroes(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    
+    vector<int> row(n, 0);
+    vector<int> col(m, 0);
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(matrix[i][j] == 0) {
+                row[i] = 1;
+                col[j] = 1;
+            }
         }
     }
-    // int row=0,col=0;
-     for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-           if(mat[i][j]==0){
-            i=0;
-            j=0;
-           }
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(row[i] == 1 || col[j] == 1) {
+                matrix[i][j] = 0;
+            }
         }
     }
-     for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-            cout<<mat[i][j]<<" ";
+}
+
+int main() {
+    int n, m;
+
+    cout << "Enter number of rows: ";
+    cin >> n;
+    cout << "Enter number of columns: ";
+    cin >> m;
+    
+    vector<vector<int>> matrix(n, vector<int>(m));
+    
+    cout << "Enter matrix elements:\n";
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> matrix[i][j];
         }
     }
+    setZeroes(matrix);
+    cout << "\nMatrix after setting zeroes:\n";
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }   
+    return 0;
 }
